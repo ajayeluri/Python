6934264f4ca7e3da7fd06083ed15e0d56b87ba52
gayathri-eigertech <gyedupati@lainternal.com>
2020-07-19 22:02:59 +0530

    tuple

---
+++ DataType_tuple.py
@@ -0,0 +1,12 @@
+#tuple consist of values sepearated by comma and can be hetogenious.
+
+#creating empty tuple
+t =()
+
+t =("hi",3,5,6)
+y =t,("hello",1,2)
+print(y)
+
+#Tuples are immutable
+#t[0] ="hi how are you?"
+
